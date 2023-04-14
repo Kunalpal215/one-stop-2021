cf230d39615930aea317ef1d794e7fe0b267bad6
Priyanshu Srivastava <priyanshusrivastava42@gmail.com>
2023-04-14 12:44:45 +0530

    added send to all endpoint

--- api/javascript/controllers/notificationController.js
+++ api/javascript/controllers/notificationController.js
@@ -49,4 +49,52 @@ exports.sendToDevice = async (req, res, next) => {
   }
 };
 
-exports.sendToAll = async (req, res, next) => {};
+exports.sendToAll = async (req, res, next) => {
+  if (!firebase.apps.length)
+    firebase.initializeApp({
+      credential: firebase.credential.cert(serviceAccount),
+    });
+
+  try {
+    if (!req.body.sendTo) {
+      throw "Missing Fields";
+    }
+
+    let user = await onestopUserModel.findOne({ email: req.body.sendTo });
+
+    if (!user) {
+      throw "Device for the given user not found!";
+    }
+    const token = user["deviceToken"];
+
+    const payload = {
+      data: {
+        category: req.body.notif.category,
+        model: req.body.notif.model,
+        header: req.body.notif.header,
+        body: req.body.notif.body,
+      },
+      topic: 'all'
+    };
+
+   
+
+    let data = await firebase.messaging().send(payload);
+
+    res.send({
+      success: true,
+      message: data,
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({
+      success: false,
+      message: e,
+    });
+  }
+
+
+
+
+
+};
